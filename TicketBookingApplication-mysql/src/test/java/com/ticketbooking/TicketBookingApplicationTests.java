4d6f2eccc8443f03e61bf97257b0e088495bdb2d
ravi7047 <65459052+ravi7047@users.noreply.github.com>
2025-02-02 08:36:06 +0530

    Ticket Booking Application
    With MY SQL Configurations.

---
+++ TicketBookingApplication-mysql/src/test/java/com/ticketbooking/TicketBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketbooking;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
