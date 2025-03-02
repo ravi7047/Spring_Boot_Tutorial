6762b0c8de35d06dcde6de2143691eee2c71a3fa
ravi7047 <65459052+ravi7047@users.noreply.github.com>
2025-03-02 12:12:10 +0530

    DB Microservices

---
+++ MicroServiceArchitecture/src/main/java/com/springboot/stock/resource/DbServiceResource.java
@@ -0,0 +1,94 @@
+package com.springboot.stock.resource;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springboot.stock.model.Quote;
+import com.springboot.stock.model.Quotes;
+import com.springboot.stock.repository.QuotesRepository;
+
+@RestController
+@RequestMapping("/rest/db")
+public class DbServiceResource {
+
+	@Autowired
+	private QuotesRepository quotesRepository;
+
+	@GetMapping("/{username}")
+	public List<String> getQuotes(@PathVariable("username") final String username) {
+
+		return getQuotesByUserName(username);
+
+	}
+	private List<String> getQuotesByUserName(@PathVariable("username") String username){
+		return quotesRepository.findByUserName(username).
+
+				/*
+				 * 	1    suniljoshi  ril
+					2   suniljoshi   hdfc
+					3    suniljoshi   jio
+
+
+				 */
+				stream().map(quote -> {
+										return quote.getQuote(); //ril,hdfc,jio
+									   }).
+
+						collect(Collectors.toList());
+	}
+
+	@PostMapping("/add")
+	public List<String> add(@RequestBody final Quotes quotes) {
+
+		/*
+		 *
+		 * 	1    suniljoshi  ril
+		2   suniljoshi   hdfc
+		3    suniljoshi   jio
+
+		SunilJoshi   ril,hdfc,jio
+		Quotes qutoes=new Quotes();
+
+		List<String> quoteslist=new ArrayList<>();
+				quoteslist.add("ril");
+				quoteslist.add("HDFC");
+				quoteslist.add("JIO");
+		qutoes.setUserName("suniljoshi");
+		qutoes.setQuotes(quoteslist);
+		return qutoes;
+
+		*/
+		//Iterating over FE Object,Mapping Database Object and Saving Database Object
+		quotes.getQuotes().stream()
+		.map(quote -> new Quote(quotes.getUserName(), quote))
+				.forEach(quote -> {
+					quotesRepository.save(quote);
+				});
+		return getQuotesByUserName(quotes.getUserName());
+		//return getQuotesByUserName(quotes.getUserName());
+	}
+
+
+
+
+
+    @PostMapping("/delete/{username}")
+    public List<String> delete(@PathVariable("username") final String username) {
+
+        List<Quote> quotes = quotesRepository.findByUserName(username);
+        quotesRepository.deleteAll(quotes);
+
+        return getQuotesByUserName(username);
+    }
+
+
+}
