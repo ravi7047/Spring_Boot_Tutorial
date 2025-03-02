1c60b74c4b469897c85cbdcf9e91812d9fc13c8f
ravi7047 <65459052+ravi7047@users.noreply.github.com>
2025-03-02 12:08:27 +0530

    Enable Eureka Client for a Microservices Example.
    Enable Eureka Client for a Microservices Example using the Histicx command

---
+++ EmployeeMainService/src/main/java/com/hystrix/example/employee/controller/EmployeeServiceController.java
@@ -0,0 +1,45 @@
+package com.hystrix.example.employee.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hystrix.example.employee.beans.Employee;
+
+@RestController
+public class EmployeeServiceController {
+	
+	//http://localhost:8011/findEmployeeDetails/111 -> Employee1
+	
+	private static final Map<Integer, Employee> employeeData = new HashMap<Integer, Employee>() {
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = -3970206781360313502L;
+
+		{
+			put(111,new Employee(111,"Employee1"));
+			put(222,new Employee(222,"Employee2"));
+			put(333,new Employee(333,"Employee3"));
+		}
+ 
+    };
+ 
+    @RequestMapping(value = "/findEmployeeDetails/{employeeId}", method = RequestMethod.GET)
+    public Employee getEmployeeDetails(@PathVariable int employeeId) {
+        System.out.println("Getting Employee details for " + employeeId);
+ 
+        Employee employee = employeeData.get(employeeId);
+        if (employee == null) {
+            
+        	employee = new Employee(0, "N/A");
+            
+        }
+        return employee;
+    }
+
+}
